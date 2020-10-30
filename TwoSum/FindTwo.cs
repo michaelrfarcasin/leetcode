45bac7bfaf66809cb44dc991132e6c3b2257b6c9
michaelrfarcasin <michaelrfarcasin@gmail.com>
2020-10-29 21:02:51 -0500

    Add project files.

---
+++ TwoSum/FindTwo.cs
@@ -0,0 +1,20 @@
+﻿namespace TwoSum
+{
+    public class FindTwo
+    {
+        public int[] SumToTarget(int[] numbers, int target)
+        {
+            for (int secondCandidate = 0; secondCandidate < numbers.Length - 1; secondCandidate++)
+            {
+                for (int candidate = secondCandidate + 1; candidate < numbers.Length; candidate++)
+                {
+                    if (numbers[secondCandidate] + numbers[candidate] == target)
+                    {
+                        return new[] { secondCandidate, candidate };
+                    }
+                }
+            }
+            return new[] { 0, 1 };
+        }
+    }
+}

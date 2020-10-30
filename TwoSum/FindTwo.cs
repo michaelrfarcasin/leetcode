3ba8dcebc4e889c22889f1692a03c2824fd7862b
michaelrfarcasin <michaelrfarcasin@gmail.com>
2020-10-30 18:04:09 -0500

    Add a better-performing algorithm

--- TwoSum/FindTwo.cs
+++ TwoSum/FindTwo.cs
@@ -1,8 +1,10 @@
-﻿namespace TwoSum
+﻿using System.Collections.Generic;
+
+namespace TwoSum
 {
     public class FindTwo
     {
-        public int[] SumToTarget(int[] numbers, int target)
+        public int[] SumToTargetNaive(int[] numbers, int target)
         {
             for (int secondCandidate = 0; secondCandidate < numbers.Length - 1; secondCandidate++)
             {
@@ -12,6 +14,23 @@
                     {
                         return new[] { secondCandidate, candidate };
                     }
+                }
+            }
+            return new[] { 0, 1 };
+        }
+
+        public int[] SumToTarget(int[] numbers, int target)
+        {
+            Dictionary<int, int> seen = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (seen.ContainsKey(target - numbers[i]))
+                {
+                    return new[] { seen[target - numbers[i]], i };
+                }
+                if (!seen.ContainsKey(numbers[i]))
+                {
+                    seen.Add(numbers[i], i);
                 }
             }
             return new[] { 0, 1 };

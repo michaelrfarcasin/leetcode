243716dfd91acd2b8bffeadf028cc3e4ba9cb37a
michaelrfarcasin <michaelrfarcasin@gmail.com>
2020-10-30 19:31:49 -0500

    Add project files.

---
+++ ThreeSum/SequenceComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ThreeSum
+{
+    class SequenceComparer<T>:IEqualityComparer<IEnumerable<T>>
+    {
+        public bool Equals(IEnumerable<T> seq1, IEnumerable<T> seq2)
+        {
+            return seq1.SequenceEqual(seq2);
+        }
+
+        public int GetHashCode(IEnumerable<T> seq)
+        {
+            int hash = 1234567;
+            foreach (T elem in seq)
+                hash = hash * 37 + elem.GetHashCode();
+            return hash;
+        }
+    }
+}

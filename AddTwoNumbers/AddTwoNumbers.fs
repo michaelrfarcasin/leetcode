7c81677c295bf240ce41b671365b15ca81b3c225
michaelrfarcasin <michaelrfarcasin@gmail.com>
2020-11-15 21:08:36 -0600

    Add project files.

---
+++ AddTwoNumbers/AddTwoNumbers.fs
@@ -0,0 +1,33 @@
+﻿module AddTwoNumbers
+
+let addWithPadding (first: int list) (second: int list) =
+    let paddingLength = second.Length - first.Length
+    let paddedFirst = 
+        if paddingLength > 0 
+        then first @ (List.init paddingLength (fun x -> 0))
+        else first
+    let paddedSecond = 
+        if paddingLength < 0 
+        then second @ (List.init (paddingLength * -1) (fun x -> 0))
+        else second
+    List.map2 (+) paddedFirst paddedSecond
+
+let removeLastIfZero (ints: int list) =
+    match List.last ints with 
+    | 0 -> ints.[.. (ints.Length - 2)]
+    | _ -> ints
+
+let rec addTensDigitsToOnesDigits =
+    function 
+    | [] -> []
+    | ints when ints.Head < 10 -> ints.Head :: (addTensDigitsToOnesDigits ints.Tail)
+    | head::[] -> [head % 10; head / 10]
+    | ints -> 
+        let newHead = ints.Head % 10
+        let tailHead = match ints.Tail with | [] -> 0 | _ -> ints.Tail.Head
+        let secondInt = (ints.Head / 10) + tailHead
+        newHead :: (addTensDigitsToOnesDigits (secondInt::ints.Tail.Tail))
+
+let addTwoNumbers (first: int list) (second: int list) =
+    let listOfSums = addWithPadding first second
+    addTensDigitsToOnesDigits listOfSums

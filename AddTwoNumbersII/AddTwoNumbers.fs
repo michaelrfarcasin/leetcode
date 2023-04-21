7b2087384ce894448063b7589629450920900e9d
michaelrfarcasin <rockdeworld@gmail.com>
2023-04-21 16:33:01 -0500

    Add LongestSubstringWithoutRepeatingCharacters

---
+++ AddTwoNumbersII/AddTwoNumbers.fs
@@ -0,0 +1,28 @@
+﻿module AddTwoNumbers
+
+let addWithPadding (first: int list) (second: int list) =
+    let paddingLength = second.Length - first.Length
+    let paddedFirst = 
+        if paddingLength > 0 
+        then first @ (List.init paddingLength (fun x -> 0))
+        else first
+    let paddedSecond = 
+        if paddingLength < 0 
+        then second @ (List.init (paddingLength * -1) (fun x -> 0))
+        else second
+    List.map2 (+) paddedFirst paddedSecond
+
+let rec addTensDigitsToOnesDigits =
+    function 
+    | [] -> []
+    | ints when ints.Head < 10 -> ints.Head :: (addTensDigitsToOnesDigits ints.Tail)
+    | head::[] -> [head % 10; head / 10]
+    | ints -> 
+        let newHead = ints.Head % 10
+        let tailHead = match ints.Tail with | [] -> 0 | _ -> ints.Tail.Head
+        let secondInt = (ints.Head / 10) + tailHead
+        newHead :: (addTensDigitsToOnesDigits (secondInt::ints.Tail.Tail))
+
+let addTwoNumbers (first: int list) (second: int list) =
+    addWithPadding (List.rev first) (List.rev second) |> addTensDigitsToOnesDigits |> List.rev 
+    

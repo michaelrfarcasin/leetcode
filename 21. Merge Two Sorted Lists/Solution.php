f3cee7ef37bbaa2bc19cf2128c19e00020199bab
michaelrfarcasin <rockdeworld@gmail.com>
2023-04-22 20:02:57 -0500

    Optimize 21

--- 21. Merge Two Sorted Lists/Solution.php
+++ 21. Merge Two Sorted Lists/Solution.php
@@ -17,42 +17,17 @@ class Solution {
      * @param ListNode $list1
      * @param ListNode $list2
      * @return ListNode
+     * @source https://leetcode.com/problems/merge-two-sorted-lists/solutions/2383569/fastest-solution-explained-0ms-100-o-n-time-complexity-o-n-space-complexity/?envType=study-plan&id=level-1&languageTags=php
      */
-    function mergeTwoLists($a, $b) {
-        if ($this->areInputsNull($a, $b)) {
-            return !$a ? $b : $a;
+    function mergeTwoLists($list1, $list2) {
+        if($list1 == null) return $list2;
+        if($list2 == null) return $list1;
+        if($list1->val < $list2->val) {
+            $list1->next = $this->mergeTwoLists($list1->next, $list2);
+            return $list1;
+        } else {
+            $list2->next = $this->mergeTwoLists($list2->next, $list1);
+            return $list2;
         }
-        if ($this->isNodeLessThan($b, $a)) {
-            $temp = $a;
-            $a = $b;
-            $b = $temp;
-        }
-        return $this->mergeTwoNonNullListsWithSmallerFirst($a, $b);
-    }
-
-    private function areInputsNull($a, $b) {
-        return $a === null || $b === null;
-    }
-
-    private function isNodeLessThan($a, $b) {
-        return $a->val < $b->val;
-    }
-
-    private function mergeTwoNonNullListsWithSmallerFirst($a, $b) {
-        $previousNode = $a;
-        while ($previousNode->next != null && $b != null) {
-            $currentNode = $previousNode->next;
-            if ($this->isNodeLessThan($b, $currentNode)) {
-                $previousNode->next = $b;
-                $b = $b->next;
-                $previousNode->next->next = $currentNode;
-            }
-            $previousNode = $previousNode->next;
-        }
-        if ($b != null) {
-            $previousNode->next = $b;
-        }
-
-        return $a;
     }
 }

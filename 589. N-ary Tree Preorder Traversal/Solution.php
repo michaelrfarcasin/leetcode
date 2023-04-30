f041c98b002135b21b3346c0fd3556779a53ba79
michaelrfarcasin <rockdeworld@gmail.com>
2023-04-30 16:14:37 -0500

    Add 589 iterative solution

--- 589. N-ary Tree Preorder Traversal/Solution.php
+++ 589. N-ary Tree Preorder Traversal/Solution.php
@@ -18,19 +18,35 @@ class Solution {
      * @return integer[]
      */
     function preorder($root) {
-        return $this->preorderWithRecursion($root, []);
+        return $this->preorderWithIteration($root);
     }
 
     /**
      * @source https://leetcode.com/problems/n-ary-tree-preorder-traversal/solutions/872903/php-recursive-n-ary-tree-preorder-traversal/
      */
-    private function preorderWithRecursion($node, $result) {
+    private function preorderWithRecursion($node, $result = []) {
         if (!isset($node)) {
             return $result;
         }
         $result[] = $node->val;
         foreach ($node->children as $child) {
             $result = $this->preorderWithRecursion($child, $result);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @source https://leetcode.com/problems/n-ary-tree-preorder-traversal/solutions/872903/php-recursive-n-ary-tree-preorder-traversal/
+     */
+    private function preorderWithIteration($root) {
+        if (!isset($root)) {
+            return [];
+        }
+        $stack = [$root];
+        while ($node = array_shift($stack)) {
+            $result[] = $node->val;
+            array_unshift($stack, ...$node->children);
         }
 
         return $result;

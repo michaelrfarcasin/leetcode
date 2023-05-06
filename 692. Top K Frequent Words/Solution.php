108c7a719795c51655e5a4512e11a7235086f9c2
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-06 18:44:10 -0500

    Clean up 692

--- 692. Top K Frequent Words/Solution.php
+++ 692. Top K Frequent Words/Solution.php
@@ -6,46 +6,39 @@ class Solution {
      * @param String[] $words
      * @param Integer $k
      * @return String[]
+     * @source https://leetcode.com/problems/top-k-frequent-words/solutions/2722266/php-solution-not-the-best-but-works/
      */
     function topKFrequent($words, $k) {
         $frequencies = $this->getSortedFrequencies($words);
-        return array_keys(array_slice($frequencies, 0, $k));
+        $wordsByFrequency = $this->getWordsByFrequency($frequencies);
+        foreach ($wordsByFrequency as $key => $words) {
+            sort($words);
+            $wordsByFrequency[$key] = $words;
+        }
+        $sortedWords = $this->flatten($wordsByFrequency);
+        return array_slice($sortedWords, 0, $k);
     }
 
     private function getSortedFrequencies($inputs) {
-        $frequencies = $this->getFrequenciesByInput($inputs);
+        $frequencies = array_count_values($inputs);
         arsort($frequencies);
-        $frequencies = $this->sortSameFrequenciesByKey($frequencies);
 
         return $frequencies;
     }
 
-    private function getFrequenciesByInput($inputs) {
-        $frequencies = [];
-        foreach ($inputs as $input) {
-            $frequencies[$input]++;
+    private function getWordsByFrequency($frequencies) {
+        $wordsByFrequency = [];
+        foreach ($frequencies as $word => $frequency) {
+            $wordsByFrequency[$frequency][] = $word;
         }
 
-        return $frequencies;
+        return $wordsByFrequency;
     }
 
-    private function sortSameFrequenciesByKey($frequencies) {
-        $sorted = $unsorted = [];
-        $currentFrequency = reset($frequencies);
-        foreach ($frequencies as $input => $frequency) {
-            if ($currentFrequency == $frequency) {
-                $unsorted[$input] = $frequency;
-                continue;
-            }
-            ksort($unsorted, SORT_STRING);
-            $sorted += $unsorted;
-            $unsorted = [$input => $frequency];
-            $currentFrequency = $frequency;
-        }
-        ksort($unsorted);
-        $sorted += $unsorted;
-        $unsorted = [];
-
-        return $sorted;
+    /** @source https://stackoverflow.com/a/1320156/2037065 */
+    private function flatten(array $array) {
+        $return = array();
+        array_walk_recursive($array, function($a) use (&$return) { $return[] = $a; });
+        return $return;
     }
 }

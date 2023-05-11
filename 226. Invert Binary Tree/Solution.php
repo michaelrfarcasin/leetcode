49b9081ce0d6c0441b96589f9f598cfbbf88fb5b
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-10 20:08:47 -0500

    Add 226

---
+++ 226. Invert Binary Tree/Solution.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($val = 0, $left = null, $right = null) {
+ *         $this->val = $val;
+ *         $this->left = $left;
+ *         $this->right = $right;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return TreeNode
+     */
+    function invertTree($root) {
+        if ($root === null) {
+            return $root;
+        }
+        $temp = $root->left;
+        $root->left = $root->right;
+        $root->right = $temp;
+        $this->invertTree($root->left);
+        $this->invertTree($root->right);
+
+        return $root;
+    }
+}

78f6cf3032b5679290cfccba4aabe32be86b0cb0
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-13 16:23:11 -0500

    Optimize 74

--- 74. Search a 2D Matrix/Solution.php
+++ 74. Search a 2D Matrix/Solution.php
@@ -6,53 +6,22 @@ class Solution {
      * @param Integer[][] $matrix
      * @param Integer $target
      * @return Boolean
+     * @source https://leetcode.com/problems/search-a-2d-matrix/solutions/26220/don-t-treat-it-as-a-2d-matrix-just-treat-it-as-a-sorted-list
      */
     function searchMatrix($matrix, $target) {
-        $targetRow = $this->getTargetRow($matrix, $target);
-        if ($targetRow < 0) {
-            return false;
-        }
-        return $this->isInRow($matrix[$targetRow], $target);
-    }
-
-    private function getTargetRow($matrix, $target) {
-        $low = 0;
-        $high = count($matrix) - 1;
-        if ($low == $high) {
-            return $low;
-        }
-        while ($low <= $high) {
-            $mid = intdiv($low + $high, 2);
-            if ($matrix[$mid][0] <= $target && (
-                $matrix[$mid + 1] === null || $target < $matrix[$mid + 1][0]
-                )) {
-                return $mid;
-            }
-            if ($matrix[$mid + 1][0] <= $target) {
-                $low = $mid + 1;
+        $numberRows = count($matrix);
+        $numberColumns = count($matrix[0]);
+        $left = 0;
+        $right = $numberRows * $numberColumns - 1;
+        while ($left != $right) {
+            $mid = ($left + $right - 1) >> 1;
+            if ($matrix[$mid / $numberColumns][$mid % $numberColumns] < $target) {
+                $left = $mid + 1;
             } else {
-                $high = $mid - 1;
+                $right = $mid;
             }
         }
 
-        return -1;
-    }
-
-    private function isInRow($row, $target) {
-        $low = 0;
-        $high = count($row) - 1;
-        while ($low <= $high) {
-            $mid = intdiv($low + $high, 2);
-            if ($row[$mid] === $target) {
-                return true;
-            }
-            if ($row[$mid] < $target) {
-                $low = $mid + 1;
-            } else {
-                $high = $mid - 1;
-            }
-        }
-
-        return false;
+        return $matrix[$right / $numberColumns][$right % $numberColumns] == $target;
     }
 }

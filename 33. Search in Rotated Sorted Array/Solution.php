eaa30c306fbcc46f017aca6a17126e0ec6d81431
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-13 17:19:03 -0500

    Clean up 33

--- 33. Search in Rotated Sorted Array/Solution.php
+++ 33. Search in Rotated Sorted Array/Solution.php
@@ -6,8 +6,40 @@ class Solution {
      * @param Integer[] $nums
      * @param Integer $target
      * @return Integer
+     *
      */
     function search($nums, $target) {
+        return $this->searchWithInfinity($nums, $target);
+    }
+
+    /** @source https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/14435/clever-idea-making-it-simple/ */
+    private function searchWithInfinity($nums, $target) {
+        $low = 0;
+        $high = count($nums);
+        while ($low < $high) {
+            $mid = intdiv($low + $high, 2);
+            $currentNumber = $nums[$mid];
+            if (!$this->onSameSideOfPivot($nums, $target, $nums[$mid])) {
+                $currentNumber = $nums[0] < $target ? INF : -INF;
+            }
+            if ($currentNumber == $target) {
+                return $mid;
+            }
+            if ($currentNumber < $target) {
+                $low = $mid + 1;
+            } else {
+                $high = $mid;
+            }
+        }
+
+        return -1;
+    }
+
+    private function onSameSideOfPivot($nums, $target, $current) {
+        return ($current < $nums[0]) == ($target < $nums[0]);
+    }
+
+    private function oldSearch($nums, $target) {
         $pivotIndex = $this->findPivotIndex($nums);
         $offset = 0;
         $length = count($nums);
@@ -27,38 +59,35 @@ class Solution {
         return $offset + $result;
     }
 
+    /** @source https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/14425/concise-o-log-n-binary-search-solution/?envType=study-plan&id=level-2&orderBy=most_votes */
     private function findPivotIndex($nums) {
         $low = 0;
-        $high = count($nums);
-        $firstNumber = reset($nums);
-        while ($low <= $high) {
-            $mid = floor(($low + $high) / 2);
-            if ($nums[$mid] > $nums[$mid + 1] && isset($nums[$mid + 1])) {
-                return $mid + 1;
-            }
-            if ($firstNumber <= $nums[$mid]) {
+        $high = count($nums) - 1;
+        while ($low < $high) {
+            $mid = intdiv($low + $high, 2);
+            if ($nums[$mid] > $nums[$high]) {
                 $low = $mid + 1;
-                continue;
+            } else {
+                $high = $mid;
             }
-            $high = $mid - 1;
         }
 
-        return -1;
+        return $low;
     }
 
     private function binarySearch($nums, $target) {
         $low = 0;
         $high = count($nums);
         while ($low <= $high) {
-            $mid = floor(($low + $high) / 2);
+            $mid = intdiv($low + $high, 2);
             if ($nums[$mid] == $target) {
                 return $mid;
             }
             if ($nums[$mid] < $target) {
                 $low = $mid + 1;
-                continue;
+            } else {
+                $high = $mid - 1;
             }
-            $high = $mid - 1;
         }
 
         return -1;

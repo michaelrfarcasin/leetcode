b66dd40cb28c73859c1f096bc4020512520c0d99
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-22 20:47:14 -0500

    76 sliding window

--- 76. Minimum Window Substring/Solution.php
+++ 76. Minimum Window Substring/Solution.php
@@ -1,5 +1,11 @@
 <?php
 
+class Window {
+    public $length = -1;
+    public $left = 0;
+    public $right = 0;
+}
+
 class Solution {
 
     /**
@@ -8,28 +14,42 @@ class Solution {
      * @return String
      */
     function minWindow($s, $t) {
-        $m = strlen($s);
-        $n = strlen($t);
-        if ($n > $m) {
+        $sLength = strlen($s);
+        $tLength = strlen($t);
+        if ($tLength > $sLength || $tLength == 0 || $sLength == 0) {
             return '';
         }
         $frequencies = array_count_values(str_split($t));
-        $left = 0; $right = $m - 1;
-        do  {
-            $leftChar = $s[$left];
-            if ($frequencies[$leftChar] > 0) {
-                $left++;
-                $frequencies[$leftChar]--;
-            }
+        $numberRequired = count($frequencies);
+        $numberFormed = 0;
+        $windowCounts = [];
+        $minWindow = new Window();
+        $left = $right = 0;
+        while ($right < $sLength) {
             $rightChar = $s[$right];
-            if ($frequencies[$rightChar] > 0) {
-                $right--;
-                $frequencies[$rightChar]--;
+            $windowCounts[$rightChar]++;
+            if (isset($frequencies[$rightChar]) && $windowCounts[$rightChar] === $frequencies[$rightChar]) {
+                $numberFormed++;
             }
-        } while (($right - $left + 1 > $n) && (
-            $frequencies[$leftChar] != 0 || $frequencies[$rightChar] != 0
-        ));
+            while ($left <= $right && $numberFormed == $numberRequired) {
+                $leftChar = $s[$left];
+                if ($minWindow->length == -1 || $right - $left + 1 < $minWindow->length) {
+                    $minWindow->length = $right - $left + 1;
+                    $minWindow->left = $left;
+                    $minWindow->right = $right;
+                }
 
-        return substr($s, $left, $right - $left + 1);
+                $windowCounts[$leftChar]--;
+                if (isset($frequencies[$leftChar]) && $windowCounts[$leftChar] < $frequencies[$leftChar]) {
+                    $numberFormed--;
+                }
+
+                $left++;
+            }
+
+            $right++;
+        }
+
+        return $minWindow->length === -1 ? '' : substr($s, $minWindow->left, $minWindow->right + 1);
     }
 }

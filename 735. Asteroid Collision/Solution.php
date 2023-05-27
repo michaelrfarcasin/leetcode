23fa0e2e37083a6eb917f6d2c64cebe0f4fd9d62
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-26 21:02:47 -0500

    Fix 735

--- 735. Asteroid Collision/Solution.php
+++ 735. Asteroid Collision/Solution.php
@@ -5,46 +5,31 @@ class Solution {
     /**
      * @param Integer[] $asteroids
      * @return Integer[]
+     * @source Editorial
      */
     function asteroidCollision($asteroids) {
-        $escapedLeft = [];
-        $escapedRight = [];
-        $length = count($asteroids);
-        $limit = 100;
-        var_dump(empty($asteroids) ? 't' : 'f');
-        while (!empty($asteroids) && $limit > 0) {
-            var_dump("$limit: " . implode(',',$asteroids));
-            while (reset($asteroids) < 0) {
-                $limit--;
-                var_dump("$limit: " . implode(',',$asteroids));
-                $escapedLeft[] = array_shift($asteroids);
-            }
-            while (end($asteroids) > 0) {
-                $limit--;
-                var_dump("$limit: " . implode(',',$asteroids));
-                array_unshift($escapedRight, array_pop($asteroids));
-            }
-            $length = count($asteroids);
-            for ($i = 0; $i < $length; $i++) {
-                $limit--;
-                var_dump("$limit: $i : " . implode(',',$asteroids));
-                if ($asteroids[$i] == 0) {
-                    unset($asteroids[$i]);
+        $stack = [];
+        foreach ($asteroids as $asteroid) {
+            $addAsteroid = true;
+            while (!empty($stack) && $asteroid < 0 && 0 < end($stack)) {
+                $asteroidSizeDifference = abs($asteroid) <=> abs(end($stack));
+                if ($asteroidSizeDifference > 0) {
+                    array_pop($stack);
                     continue;
                 }
-                if ($i == $length - 1 && $asteroids[$i] > 0) {
+                if ($asteroidSizeDifference == 0) {
+                    array_pop($stack);
+                    $addAsteroid = false;
                     break;
                 }
-                if ($asteroids[$i] > 0) {
-                    $asteroids[$i + 1] = max($asteroids[$i], $asteroids[$i + 1]);
-                    $asteroids[$i] = 0;
-                } else {
-                    $asteroids[$i - 1] = max($asteroids[$i], $asteroids[$i - 1]);
-                    $asteroids[$i] = 0;
-                }
+                $addAsteroid = false;
+                break;
+            }
+            if ($addAsteroid) {
+                array_push($stack, $asteroid);
             }
         }
 
-        return array_merge($escapedLeft, $escapedRight);
+        return $stack;
     }
 }

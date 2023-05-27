8584bc2dcdde13eee28d918c078437254f1056aa
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-27 16:59:53 -0500

    547 with UnionFind

--- 547. Number of Provinces/Solution.php
+++ 547. Number of Provinces/Solution.php
@@ -1,5 +1,41 @@
 <?php
 
+class UnionFind {
+    public array $parent = [];
+    public array $rank = [];
+
+    public function __construct(int $size) {
+        for ($i = 0; $i < $size; $i++) {
+            $this->parent[$i] = $i;
+        }
+        $this->rank = array_fill(0, $size, 0);
+    }
+
+    public function find(int $x): int {
+        if ($this->parent[$x] != $x) {
+            $this->parent[$x] = $this->find($this->parent[$x]);
+        }
+
+        return $this->parent[$x];
+    }
+
+    public function unionSet(int $x, int $y): void {
+        $xSet = $this->find($x);
+        $ySet = $this->find($y);
+        if ($xSet == $ySet) {
+            return;
+        }
+        if ($this->rank[$xSet] < $this->rank[$ySet]) {
+            $this->parent[$xSet] = $ySet;
+        } elseif ($this->rank[$xSet] > $this->rank[$ySet]) {
+            $this->parent[$ySet] = $xSet;
+        } else {
+            $this->parent[$ySet] = $xSet;
+            $this->rank[$xSet]++;
+        }
+    }
+}
+
 class Solution {
 
     /**
@@ -7,34 +43,18 @@ class Solution {
      * @return Integer
      */
     function findCircleNum($isConnected) {
-        $cityToProvince = [];
-        $provinceToCity = [];
         $numberCities = count($isConnected);
+        $dsu = new UnionFind($numberCities);
+        $numberOfComponents = $numberCities;
         for ($i = 0; $i < $numberCities; $i++) {
-            $cityToProvince[$i] = $i;
-            $provinceToCity[$i][$i] = $i;
-        }
-        for ($i = 1; $i < $numberCities; $i++) {
-            for ($j = 0; $j < $i; $j++) {
-                if (!$isConnected[$i][$j] || $cityToProvince[$i] == $cityToProvince[$j]) {
-                    continue;
+            for ($j = $i + 1; $j < $numberCities; $j++) {
+                if ($isConnected[$i][$j] == 1 && $dsu->find($i) != $dsu->find($j)) {
+                    $numberOfComponents--;
+                    $dsu->unionSet($i, $j);
                 }
-                $province = $cityToProvince[$j];
-                $otherProvince = $cityToProvince[$i];
-                foreach ($provinceToCity[$otherProvince] as $city) {
-                    $provinceToCity[$province][$city] = $city;
-                    $cityToProvince[$city] = $province;
-                }
-                unset($provinceToCity[$otherProvince]);
             }
         }
 
-        return count($provinceToCity);
-    }
-
-    private function dumpProvinces($provinceToCity) {
-        foreach ($provinceToCity as $province => $cities) {
-            var_dump("    $province => ". implode(',',$cities));
-        }
+        return $numberOfComponents;
     }
 }

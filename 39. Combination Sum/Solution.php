4c6111c937484215e69b4f0bb9604c9d4a671d79
michaelrfarcasin <rockdeworld@gmail.com>
2023-05-28 14:00:37 -0500

    Add 39

--- 39. Combination Sum/Solution.php
+++ 39. Combination Sum/Solution.php
@@ -7,25 +7,25 @@ class Solution {
      * @param Integer[] $candidates
      * @param Integer $target
      * @return Integer[][]
+     * @source discussion
      */
     function combinationSum($candidates, $target) {
-        $keyedCandidates = array_combine($candidates, $candidates);
-        $this->getCombinations($keyedCandidates, $target, []);
+        sort($candidates);
+        $this->getCombinations($candidates, $target, []);
 
         return $this->sums;
     }
 
     private function getCombinations($candidates, $target, $combination) {
-        if ($candidates[$target]) {
-            $combination[] = $target;
+        if ($target == 0) {
             sort($combination);
             $this->sums[implode(',',$combination)] = $combination;
             return;
         }
-        if ($target < 2 || empty($candidates)) {
-            return;
-        }
         foreach ($candidates as $candidate) {
+            if ($target - $candidate < 0) {
+                break;
+            }
             $this->getCombinations($candidates, $target - $candidate, array_merge($combination, [$candidate]));
         }
     }
